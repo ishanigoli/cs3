9ed98d61836ad9cc9cbe39e5d17ffe984587b29e
ishanigoli <ishanigoli@gmail.com>
2026-09-08 08:52:46 -0500

    Update ExpressionSolver.java

--- u02a_arraylists/labs/04e - ExpressionSolver/ExpressionSolver.java
+++ u02a_arraylists/labs/04e - ExpressionSolver/ExpressionSolver.java
@@ -12,19 +12,28 @@ import static java.lang.System.*;
 public class ExpressionSolver
 {
 	private ArrayList<String> expression;
+   private String exp;
    //add in instance variables
 
 	public ExpressionSolver(String s)
 	{
-      
+      setExpression(s);
 	}
 
 	public void setExpression(String s)
 	{
+      exp = s;
+      for(int i = 0; i < exp.length; i++) {
+         expression.add(exp.substring(i, i+1));
+      }
 	}
 
 	public void solveExpression()
-	{
+	{ 
+      while(s.indexOf("*") != -1 || s.indexO("/") != -1) {
+         for(int i = 0; i < expression.size(); i++) {
+            
+            
 	}
 
 	public String toString( )
